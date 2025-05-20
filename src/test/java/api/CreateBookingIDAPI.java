695026d5b2ca7949c121f327df16ad3553ee1dec
rvmalikrm <rm.rvmalik@gmail.com>
2025-05-20 15:57:54 +0530

    Second commit

--- src/test/java/api/CreateBookingIDAPI.java
+++ src/test/java/api/CreateBookingIDAPI.java
@@ -1,4 +1,37 @@
 package api;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class CreateBookingIDAPI {
+    @Test
+
+    public void testCreateBookingID(){
+        RestAssured.baseURI= "https://restful-booker.herokuapp.com";
+
+        String payload= """
+                {
+                    "firstname" : "Pooja",
+                    "lastname" : "Malik",
+                    "totalprice" : 222,
+                    "depositpaid" : true,
+                    "bookingdates" : {
+                        "checkin" : "2018-01-01",
+                        "checkout" : "2019-01-01"
+                    },
+                    "additionalneeds" : "Breakfast"
+                }""";
+        Response response= RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .body(payload)
+                .when()
+                .log().all().post("/booking");
+        response.prettyPrint();
+        Assert.assertEquals(response.getStatusCode(),200);
+
+    }
 }
--- src/test/java/api/PostApiTest.java
+++ src/test/java/api/PostApiTest.java
@@ -1,4 +1,34 @@
 package api;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class PostApiTest {
+    @Test
+    public void createTokenTest(){
+        // Base URI
+        RestAssured.baseURI= "https://restful-booker.herokuapp.com";
+        // Payload
+        String payload= """
+                {
+                    "username" : "admin",
+                    "password" : "password123"
+                }
+                """;
+        // Send Post request
+        Response response = RestAssured
+                .given()
+                    .contentType(ContentType.JSON)
+                    .body(payload)
+                .when()
+                .log().all().post("/auth");
+        // Print Response
+        response.prettyPrint();
+
+        // Validation
+        Assert.assertEquals(response.getStatusCode(), 200);
+    }
 }
